3ef81933e2839e9ac055b3c75952245c06bf6a8b
DingPengCheng <2504488720@qq.com>
2020-03-29 23:15:49 +0800

    Main Scene Status Transition and Display
    主界面状态栏显示和跳转到状态场景

--- Client/Exermon/Assets/Scripts/Controls/MainScene/StatusDisplay.cs
+++ Client/Exermon/Assets/Scripts/Controls/MainScene/StatusDisplay.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using PlayerModule.Data;
 
 using Core.UI;
 
 using PlayerModule.Data;
 using PlayerModule.Services;
+using UI.Common.Controls.ParamDisplays;
 
 namespace UI.MainScene.Controls {
 
@@ -20,7 +22,10 @@ namespace UI.MainScene.Controls {
         /// <summary>
         /// 外部组件设置
         /// </summary>
-
+        public Image face;
+        public Text name;
+        public Text battlePoint;
+        public Text level;
         /// <summary>
         /// 内部变量设置
         /// </summary>
@@ -43,6 +48,19 @@ namespace UI.MainScene.Controls {
         /// <param name="player">玩家</param>
         void drawPlayerStatus(Player player) {
             // 具体绘制代码
+            // 绘制头像
+            var character = player.character();
+            var face = character.face;
+            var rect = new Rect(0, 0, face.width, face.height);
+            this.face.gameObject.SetActive(true);
+            this.face.overrideSprite = Sprite.Create(
+                face, rect, new Vector2(0.5f, 0.5f));
+            this.face.overrideSprite.name = face.name;
+            // 设置状态栏上的等级，战斗力等信息
+            name.text = player.name;
+            level.text = player.level.ToString();
+            var exerSlot = player.slotContainers.exerSlot;
+            battlePoint.text = exerSlot.sumBattlePoint().ToString();
         }
 
         /// <summary>
