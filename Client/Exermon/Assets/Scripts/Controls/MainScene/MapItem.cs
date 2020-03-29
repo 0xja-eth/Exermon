249a931f8eb5b6c5b3e87fe857bf823ca6d0afc2
DingPengCheng <2504488720@qq.com>
2020-03-29 10:43:11 +0800

    Main scene chatroom and map animation

---
+++ Client/Exermon/Assets/Scripts/Controls/MainScene/MapItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MapItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    Animator anim;
+    private void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+    void Update()
+    {
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("enter", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("enter", false);
+    }
+}
