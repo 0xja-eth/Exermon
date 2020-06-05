593639c802718a9a5838603af2f1363b81c6b46d
JunhuiLee <861921648@qq.com>
2020-06-05 20:11:13 +0800

    更新一下商人据点中的卡牌展示样式。

--- Client/Exermon/Assets/Scripts/Controls/ExerPro/English/BusinessManScene/CardItem/CardItemDisplay.cs
+++ Client/Exermon/Assets/Scripts/Controls/ExerPro/English/BusinessManScene/CardItem/CardItemDisplay.cs
@@ -1,9 +1,27 @@
 ﻿using ExerPro.EnglishModule.Data;
-
+using UnityEngine;
+using UnityEngine.UI;
 namespace UI.ExerPro.BusinessManScene.Controls.CardItem
 {
     /// <summary>
     /// 装备背包显示
     /// </summary>
-    public class CardItemDisplay : ShopItemDisplay<ExerProCard> { }
+    public class CardItemDisplay : ShopItemDisplay<ExerProCard> {
+        /// <summary>
+        /// 外部组件定义
+        /// </summary>
+        public Text cost;
+        public Text description;
+
+        /// <summary>
+        /// 绘制基本信息
+        /// </summary>
+        /// <param name="item">物品</param>
+        protected override void drawBaseInfo(ExerProCard item)
+        {
+            base.drawBaseInfo(item);
+            cost.text = item.cost.ToString();
+            description.text = item.description;
+        }
+    }
 }
