7e7310ae5aaf9ca80653d003ae9e7fbcaa367f49
JunhuiLee <861921648@qq.com>
2020-06-26 17:19:42 +0800

    处理merge后一些运行错误

--- Client/Exermon/Assets/Scripts/Config.cs
+++ Client/Exermon/Assets/Scripts/Config.cs
@@ -8,18 +8,18 @@ using Core.Systems;
 /// </summary>
 public class Config {
 
-	/// <summary>
-	/// 服务器地址
-	/// </summary>
-	//public const string ServerURL = "ws://111.230.227.84:8001/game/";
-	//public const string ServerURL = "ws://111.230.227.84:8002/game/";            
-	//public const string ServerURL = "ws://120.79.176.90:8001/game/";
-	public const string ServerURL = "ws://127.0.0.1:8000/game/";
+    /// <summary>
+    /// 服务器地址
+    /// </summary>
+    //public const string ServerURL = "ws://111.230.227.84:8001/game/";
+    //public const string ServerURL = "ws://111.230.227.84:8002/game/";            
+    public const string ServerURL = "ws://120.79.176.90:8001/game/";
+    //public const string ServerURL = "ws://127.0.0.1:8000/game/";
 
-	/// <summary>
-	/// 前端主版本号
-	/// </summary>
-	public const string LocalMainVersion = "0.3.2";
+    /// <summary>
+    /// 前端主版本号
+    /// </summary>
+    public const string LocalMainVersion = "0.3.2";
 
 	/// <summary>
 	/// 前端副版本号
